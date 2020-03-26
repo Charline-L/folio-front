e5b6faef1338a7f7489b45b48e0dc6018c2744d4
Charline <charline.laporte@r3lief.fr>
2020-03-26 23:22:47 +0100

    ADD: premier composant + sass + font + reset

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="g-flux--large">
+    <CircleAndText 
+      firstMessage="Bonjour Charline !"
+      secondMessage="Développeuse web."
+      emoji="👋"
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CircleAndText from './components/CircleAndText.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CircleAndText
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="sass">
+
+#app
+  color: $black
+  background-color: $powder
+  padding-top: 10rem
 </style>
---
+++ src/components/CircleAndText.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="c-circle-and-text">
+    <span class="c-circle-and-text__emoji">
+      {{ emoji }}
+    </span>
+    <div class="c-circle-and-text__container-texts">
+      <p>{{ firstMessage }}</p>
+      <p v-if="secondMessage">{{ secondMessage }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CircleAndText',
+  props: {
+    firstMessage: String,
+    secondMessage: String,
+    emoji: String,
+  }
+}
+</script>
+
+<style lang="sass">
+.c-circle-and-text
+  display: flex
+  align-items: center
+
+  &__emoji
+    +size(20rem)
+    display: flex
+    justify-content: center
+    align-items: center
+    background-color: $black
+    border-radius: 100%
+    font-size: 5rem
+    padding-top: 1rem  
+    margin-right: 8rem
+
+  &__container-texts
+    font-family: $bold
+    font-size: 3.2rem
+    line-height: 5rem
+
+</style>
